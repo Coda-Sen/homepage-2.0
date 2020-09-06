6337478193cbad49337315f4bfc940d22a9688c2
Coda-Sen <60877150+Coda-Sen@users.noreply.github.com>
2020-09-06 18:36:31 +0200

    added JS file and code for a sidebar

---
+++ coda.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementsByClassName("sidenav").style.width = "384px";
+}
+function closeNav() {
+    document.getElementsByClassName("sidenav").style.width = "0px";
+}
